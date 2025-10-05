20db5e70b7d70d914f9a2b6d973a39b4b8528f08
Gorthaur <demon3xv@mail.ru>
2025-10-06 02:27:42 +0300

    срфт

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import './styles/tw.css';
 import './styles/glance-theme.css';
 import './styles/cart-drawer.css';
 import './styles/product-checkout.css';
